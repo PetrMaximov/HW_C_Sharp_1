02b144e7cdef7298efe695ae35e34b8e06b1939a
«Petr» <maxpetrb@mail.ru>
2023-09-04 23:02:39 +0300

    Task8 вывод всех четных чисел до N

---
+++ Task8/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+int rem = N % 2;
+
+if (rem != 0) N = N-1;
+
+int Nrem = N / 2;
+int[] even = new int[Nrem];
+
+
+int i = 0;
+
+while (i < Nrem)
+    {
+        even[i] = N;
+        N = N - 2;
+        i = i + 1;
+    }
+
+
+Console.WriteLine("Четные числа: ");
+var str = string.Join(" ", even);
+Console.WriteLine(str);
