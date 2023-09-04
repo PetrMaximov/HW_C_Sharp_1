35f62f306b64c68b2b35c85c4cc0a2966f710760
«Petr» <maxpetrb@mail.ru>
2023-09-04 21:50:55 +0300

    Task6 определение четности числа

---
+++ Task6/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+int rem = numberA % 2;
+if (rem == 0) 
+{
+    Console.Write (numberA);
+    Console.WriteLine (" - четное");
+}
+else
+{
+    Console.Write (numberA);
+    Console.WriteLine (" - нечетное");
+}
